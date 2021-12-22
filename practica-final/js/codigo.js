a254827ed29b68116afd309e8c0bdfa76b53dd45
reyesmouriz <re26ster@gmail.com>
2021-12-22 14:33:43 +0100

    Cambio javascript

--- practica-final/js/codigo.js
+++ practica-final/js/codigo.js
@@ -6,18 +6,47 @@ if (document.addEventListener) {
 
 function inicio(){
     //Aquí se empezaría a ejecutar nuestro código al finalizar la carga del DOM
+    
 }
 
 function mostrarNombres(){
-    var nombres = document.querySelector("#nombres").value;
-    var arrayNombres = nombres.split(",");
-    console.log(arrayNombres);
+    var arrayNombres = [];
+    var arrayRepetido = [];
+
+    var nombres1 = document.querySelector("#lst1").innerHTML;
+    var nombres2 = document.querySelector("#lst2").innerHTML;
+    var nombres3 = document.querySelector("#lst3").innerHTML;
+    var nombres4 = document.querySelector("#lst4").innerHTML;
+    var nombres5 = document.querySelector("#lst5").innerHTML;
+
+    arrayNombres.push(nombres1);
+    arrayNombres.push(nombres2);
+    arrayNombres.push(nombres3);
+    arrayNombres.push(nombres4);
+    arrayNombres.push(nombres5);
+
     
-    for (var i=0; i<=arrayNombres.length-1; i++) {
+    
+    
+    //var arrayNombres = nombres.split(",");
+    //arrayNombres.push(nombres);
+    
+    for (var i=0; i< arrayNombres.length; i++) {
+        
+        var numero = pickRandom(arrayNombres.length);
+        
         arrayNombres[i] = arrayNombres[i].trim();
+        arrayRepetido.push(arrayNombres[i]);
+        
+
+        if(arrayNombres[i]!= arrayNombres[numero])
+        {
+            console.log(arrayNombres[i] + " regala a " + arrayNombres[numero]);
+            
+        } 
+        
     }
 
-    console.log(pickRandom(arrayNombres.length));
 
 }
 
@@ -25,3 +54,5 @@ function pickRandom(limite){
     var azar = Math.floor(Math.random()*limite);
     return azar;
 }
+
+
